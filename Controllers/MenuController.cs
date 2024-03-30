8f380fb030f085baf2c3edfc764ef6d9193a3dd1
yamini-nagireddy <yaminireddy3000@gmail.com>
2024-03-29 20:01:56 -0700

    first commit

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodMenuApp.Data;
+using FoodMenuApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodMenuApp.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly MenuContext _context;
+        public MenuController(MenuContext context) 
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var dishes = from d in _context.Dishes
+                       select d;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                dishes = dishes.Where(d => d.Name.ToLower().Contains(searchString.ToLower()));
+                return View(await dishes.ToListAsync());
+
+
+            }
+            return View( await dishes.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details (int? id)
+        {
+            var dish = await _context.Dishes
+                .Include(di => di.DishIngredients)
+                .ThenInclude(i => i.Ingredient)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (dish == null) 
+            {
+                return NotFound();
+            }
+            return View(dish);
+        }
+    }
+}
---
+++ Data/MenuContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FoodMenuApp.Models;
+
+namespace FoodMenuApp.Data
+{
+    public class MenuContext : DbContext
+    {
+        public MenuContext( DbContextOptions<MenuContext> options ) : base(options) 
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DishIngredient>().HasKey(di => new
+            {
+                di.DishId,
+                di.IngredientId
+            });
+            //one dish has many dishIngrediets
+            //one ingreditent has many dishIngredients
+            modelBuilder.Entity<DishIngredient>().HasOne(d => d.Dish).WithMany(di => di.DishIngredients).HasForeignKey(d => d.DishId);
+            modelBuilder.Entity<DishIngredient>().HasOne(i => i.Ingredient).WithMany(di => di.DishIngredients).HasForeignKey(i => i.IngredientId);
+
+            //Seed data
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id=1, Name= "Margheritta", Price= 7.50, ImageUrl= "https://cdn.shopify.com/s/files/1/0205/9582/articles/20220211142347-margherita-9920_ba86be55-674e-4f35-8094-2067ab41a671.jpg?crop=center&height=915&v=1644590192&width=1200" },
+                new Dish { Id=2, Name= "Pepperoni", Price= 8.50, ImageUrl= "https://www.shutterstock.com/image-photo/tasty-pepperoni-pizza-cooking-ingredients-260nw-1239982861.jpg" }
+                );
+            modelBuilder.Entity<Ingredient>().HasData(
+                new Ingredient { Id= 1, Name="Tomato Sauce"},
+                new Ingredient { Id = 2, Name = "Mozzarella" }
+                );
+            modelBuilder.Entity<DishIngredient>().HasData(
+                new DishIngredient { DishId=1, IngredientId=1},
+                new DishIngredient { DishId = 1, IngredientId = 2 }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Dish> Dishes { get; set; }  
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<DishIngredient> DishIngredients { get; set; }
+    }
+}
